1c31509bb0a029806ba93aa9c0b6c28faf837cdf
Ajay <ajay@saturam.com>
2025-11-27 18:35:57 +0000

    added 2025-11-28

--- airflow_home/dags/pipeline.py
+++ airflow_home/dags/pipeline.py
@@ -1,34 +1,16 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
-from datetime import datetime
-import logging
-
-
-
 import sys
 import os
+from datetime import datetime
+
 sys.path.append(os.path.join(os.path.dirname(__file__), "../../"))
-from src.extract.weather_api import ApiPuller
-
-logger = logging.getLogger(__name__)
-
-def extract_weather(**context):
-    api_url = "https://api.open-meteo.com/v1/forecast"
-    params = {
-        "latitude": 12.9716,
-        "longitude": 77.5946,
-        "hourly": "temperature_2m,relativehumidity_2m"
-    }
-
-    api = ApiPuller(api_url=api_url, params=params)
-    data = api._fetch_data(params)
-
-    logger.info(data)
+from src.extract.weather_api import extract_weather
 
 with DAG(
     dag_id='weather_data_pipeline',
-    start_date=datetime(2023, 1, 1),
+    start_date=datetime(2024, 11, 27),
     schedule_interval='30 6 * * *',
     catchup=False,
     tags=['weather', 'air_quality'],
--- src/extract/weather_api.py
+++ src/extract/weather_api.py
@@ -1,33 +1,73 @@
 import requests
 import logging
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+import pandas as pd
 
+load_dotenv()
 logger = logging.getLogger(__name__)
+
+
+class SourceConfig():
+    def __init__(self):
+        self.user = os.getenv('DB_USER')
+        self.password = os.getenv('DB_PASSWORD')
+        self.host = os.getenv('DB_HOST')
+        self.port = os.getenv('DB_PORT')
+        self.db = os.getenv('DB_NAME')
+
+    def get_connection_string(self):
+        return f"postgresql+psycopg2://{self.user}:{self.password}@{self.host}:{self.port}/{self.db}"
+
 
 class ApiPuller():
     def __init__(self, api_url: str, params: dict = None):
         self.api_url = api_url
         self.params = params
 
-    def _fetch_data(self, params: dict) -> dict:
+    def fetch_data(self) -> dict:
         headers = {
             "Accept": "application/json",
             "User-Agent": "weather-air-quality-pipeline/1.0"
         }
 
-        logger.info(f"Fetching data...")
+        logger.info("Fetching weather data...")
 
         try:
-            response = requests.get(self.api_url, headers=headers, params=params)
+            response = requests.get(self.api_url, headers=headers, params=self.params)
             response.raise_for_status()
             return response.json()
-        except requests.exceptions.ConnectTimeout:
-            logger.error("Connection timed out error while connecting to the API.")
-
-        except requests.exceptions.HTTPError as http_err:
-            logger.error(f"HTTP error occurred: {http_err}")
-
-        except requests.exceptions.RequestException as req_err:
-            logger.error(f"Request exception occurred: {req_err}")
 
         except Exception as e:
-            logger.error(f"An unexpected error occurred: {e}")           
+            logger.error(f"API error: {e}")
+            return None
+
+
+def extract_weather():
+    api_url = os.getenv('API_URL')
+
+    params = {
+        "latitude": 12.9716,
+        "longitude": 77.5946,
+        "hourly": "temperature_2m,relativehumidity_2m"
+    }
+
+    api = ApiPuller(api_url, params)
+    data = api.fetch_data()
+
+    if not data:
+        raise Exception("API returned no data")
+
+    hourly = data["hourly"]
+
+    df = pd.DataFrame({
+        "time": hourly["time"],
+        "temperature_2m": hourly["temperature_2m"],
+        "relativehumidity_2m": hourly["relativehumidity_2m"]
+    })
+
+    engine = create_engine(SourceConfig().get_connection_string())
+    df.to_sql("weather_hourly", engine, if_exists="append", index=False)
+
+    logger.info("Weather data written to Postgres successfully")
