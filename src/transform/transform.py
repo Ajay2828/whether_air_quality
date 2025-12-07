6c552edf91eb2f5106ad8903df7ee06ec0c7ba1f
Ajay <ajay@saturam.com>
2025-12-07 18:39:27 +0000

    added 2025/12/08

--- src/transform/transform.py
+++ src/transform/transform.py
@@ -20,30 +20,54 @@ class SourceConfig():
         return f"postgresql+psycopg2://{self.user}:{self.password}@{self.host}:{self.port}/{self.db}"
     
 class Tranformer():
-    def tranformation(self):
+    def tranformation_silver(self):
         engine = create_engine(SourceConfig().get_connection_string())
 
-        
         yesterday = datetime.utcnow() - timedelta(days=1)
-        datetime_str = yesterday.strftime('%Y-%m-%d %H:%M:%S')
-        sql_query = f"SELECT * FROM bronze.weather_hourly_raw WHERE time > '{datetime_str}';"
+        sql_query = f"SELECT * FROM bronze.weather_hourly_raw WHERE time > '{yesterday}';"
         df = pd.read_sql_query(sql_query, con=engine)
 
         if df.empty:
             logger.warning("No data found in the weather_hourly_raw table.")
             return
         
-        sql_query = f"SELECT * FROM bronze.air_quality_hourly_raw WHERE time > '{datetime_str}';"
+        sql_query = f"SELECT * FROM bronze.air_quality_hourly_raw WHERE time > '{yesterday}';"
         df1 = pd.read_sql_query(sql_query, con=engine)
 
         if df1.empty:
             logger.warning("No data found in the air_quality_hourly_raw table.")
             return
 
-        logger.info(df.head())
-        logger.info(df1.head())
+        df_unique = df.drop_duplicates()
+        df_unique1 = df1.drop_duplicates()
 
-# def tranform_init():
-#     transformer = Tranformer()
-#     transformer.tranformation()
-#     logger.info("Data transformation completed successfully.")
+        merged_df = pd.concat([df_unique, df_unique1], axis=1)
+
+        merged_df.to_sql('weather_air_quality_hourly', con=engine, schema='silver', if_exists='append', index=False)
+        logger.info("Data transformation and loading to silver.weather_air_quality_hourly completed successfully.")
+
+    def tranformation_gold(self):
+        engine = create_engine(SourceConfig().get_connection_string())
+        
+        yesterday = datetime.utcnow() - timedelta(days=1)
+        sql_query = f"SELECT * FROM silver.weather_air_quality_hourly  WHERE time > '{yesterday}';"
+        df = pd.read_sql_query(sql_query, con=engine)
+
+        if df.empty:
+            logger.warning("No data found in the silver.weather_air_quality_hourly table.")
+            return
+
+        df['time'] = pd.to_datetime(df['time'])
+        df['date'] = df['time'].dt.date
+        df['hour'] = df['time'].dt.hour
+
+        gold_df = df.groupby(['date', 'hour']).agg({
+            'temperature': 'mean',
+            'humidity': 'mean',
+            'pressure': 'mean',
+            'air_quality_index': 'mean'
+        }).reset_index()
+
+        gold_df.to_sql('weather_air_quality_daily', con=engine, schema='gold', if_exists='append', index=False)
+        logger.info("Data transformation and loading to gold.weather_air_quality_daily completed successfully.")
+
